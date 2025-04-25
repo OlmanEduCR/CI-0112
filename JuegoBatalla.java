70f0e5600133979d304ba84cf5f2877d65466870
Olman Eduardo Camacho Jerez <C31523@ecci.ucr.ac.cr>
2025-04-25 17:10:38 -0600

    Corrección de varios errores y progreso

--- JuegoBatalla.java
+++ JuegoBatalla.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class JuegoBatalla{
-    
+    //Atributos
+    private Robot[] arrayRobots;
+
+    //Métodos
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);        
+    }
+
+    public void iniciarBatalla(){
+        
+    }
+
+    public mostrarGanador(){
+
+    }
 }
--- Robot.java
+++ Robot.java
@@ -3,46 +3,55 @@ public class Robot{
     private String nombre; //El nombre del robot.
     private double puntosVida; //Cantidad de vida (entre 50 y 100)
     private double ataque; //Cantidad de daño que puede causar (entre 10 y 20).
+    private double defensa; //Un porcentaje del ataque que se resta del ataque (el porcentaje oscila entre 5 y 10)
 
     //Getters
     public String getNombre(){
-        return nombre;
+        return this.nombre;
     }
 
     public double getPuntosVida(){
-        return puntosVida;
+        return this.puntosVida;
     }
 
     public double getAtaque(){
-        return ataque;
+        return this.ataque;
+    }
+
+    public double getDefensa(){
+        return this.defensa;
     }
 
     //Setters
-    public void setNombre(String nombrep){
-        this.nombre = nombrep;
+    public void setNombre(String nombreP){
+        this.nombre = nombreP;
     }
 
-    public void setPuntosVida(double puntosVidap){
-        this.puntosVida = puntosVidap;
+    public void setPuntosVida(double puntosVidaP){
+        this.puntosVida = puntosVidaP;
     }
 
-    public void setAtaque(double ataquep){
-        this.ataque = ataquep;
+    public void setAtaque(double ataqueP){
+        this.ataque = ataqueP;
+    }
+
+    public void setDefensa(double defensaP){
+        this.defensa = defensaP;
     }
 
     //Método constructor
-    public Robot(String nombre, double puntosVida, double ataque){
+    public Robot(String nombre, double puntosVida, double ataque, double defensa){
         this.nombre = nombre;
         this.ataque = ataque;
         this.puntosVida = puntosVida;
+        this.defensa = defensa;
     }
 
     //Métodos
-    public double atacar(double ataqueDeTurno){
-        double puntosNuevosVida = puntosVida - ataqueDeTurno;
-        this.puntosVida = puntosNuevosVida;
-        return puntosNuevosVida;
-    }//función básica de ataque, el cual resta de la vida el ataque y el resultado lo convierte en la nueva vida
+    public void atacar(Robot otroRobot){
+        double puntosNuevosVida = otroRobot.getDefensa() - this.ataque;
+        otroRobot.setPuntosVida(puntosNuevosVida);
+    }//la función básica de ataque, la cual resta de la vida el ataque y el resultado lo convierte en la nueva vida
 
     public boolean estaVivo(){
         boolean estado = false;
@@ -50,5 +59,9 @@ public class Robot{
             estado = true;
         }
         return estado;
-    }//funcion principal es determinar el estado del robot que sea solicitado
+    }//la funcion principal es determinar el estado del robot que sea solicitado
+
+    /* public double defensa(double defensaDeTurno){
+        double puntosNuevosVida = puntosVida + (defensaDeTurno/100);
+    } */
 }
