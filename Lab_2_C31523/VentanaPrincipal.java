36db82cd2fc8b1500d9e9342736de5e03f981c86
Olman Eduardo Camacho Jerez <C31523@ecci.ucr.ac.cr>
2025-05-27 17:49:14 -0600

    Avance Laboratorio

--- Lab_2_C31523/VentanaPrincipal.java
+++ Lab_2_C31523/VentanaPrincipal.java
@@ -40,12 +40,24 @@ public class VentanaPrincipal extends JFrame{
         JLabel etiquetaTelefono = new JLabel("Teléfono:");
         JTextField campoTelefono = new JTextField(20);
 
+        //Tipo de usuario
+        JLabel etiquetaTipoUsuario = new JLabel("Tipo usuario: ");
+        String[] clasificacion = {"Cliente", "Empleado", "Administrador"};
+        JComboBox<String> tipoUsuario = new JComboBox<String>(clasificacion);
+
+        //Términos y Condiciones
+        String texto = "Aceptar Términos y Condiciones";
+        JCheckBox terminosYCondiciones = new JCheckBox(texto);
+
         panelFormulario.add(etiquetaNombre);
         panelFormulario.add(campoNombre);
         panelFormulario.add(etiquetaEmail);
         panelFormulario.add(campoEmail);
         panelFormulario.add(etiquetaTelefono);
         panelFormulario.add(campoTelefono);
+        panelFormulario.add(etiquetaTipoUsuario);
+        panelFormulario.add(tipoUsuario);
+        panelFormulario.add(terminosYCondiciones);
         add(panelFormulario);
 
         //Guardado de Datos
@@ -55,21 +67,28 @@ public class VentanaPrincipal extends JFrame{
             String nombre = campoNombre.getText();
             String email = campoEmail.getText();
             String telefono = campoTelefono.getText();
+            String tipo = (String) tipoUsuario.getSelectedItem();
 
             try {
                 FileWriter writer = new FileWriter("datos_usuario.txt", true);
-                writer.write("Nombre: " + nombre + ", Email: " + email + ",Teléfono: " + telefono + ".");
+                writer.write("Nombre: " + nombre + ", Email: " + email + ",Teléfono: " + telefono + ", Tipo: " + tipo + ".");
                 writer.close();
                 JOptionPane.showMessageDialog(null, "¡Datos guardados exitosamente!");
             } catch (IOException ex){
                 JOptionPane.showMessageDialog(null, "Error al guardar los datos.");
-            }
-        });
+            }  
+        }); panelFormulario.add(botonGuardar);
 
-        panelFormulario.add(botonGuardar);
-    }
+        //Limpiar txt
+        JButton botonLimpiar = new JButton("Limpiar");
+
+        botonLimpiar.addActionListener(e ->{
+            campoNombre.setText(null);
+            campoEmail.setText(null);
+            campoTelefono.setText(null);
+        }); panelFormulario.add(botonLimpiar);
     
-
+    }
     public static void main(String[] args){
         VentanaPrincipal ventana = new VentanaPrincipal();
         ventana.setVisible(true);
